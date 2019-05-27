61c315ef9dbdfbda4e01d95bfecb7bd0c672d456
Sam Kamenetz <samkamenetz@gmail.com>
2019-05-27 16:20:49 -0400

    fleshed out first functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,32 @@
 from selenium import webdriver
+import unittest
 import time
-browser = webdriver.Chrome('.\chromedriver.exe')
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class CasualViewerTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Chrome('.\chromedriver.exe')
+
+	def tearDown(self):
+		self.browser.quit()
+		
+	def test_can_vote_on_curses_(self):
+		#user visits the site, and sees the "cursed-economy" on the page:
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Cursed Economy', self.browser.title)
+		self.fail
+		#user presented with two options
+		
+		#each curse option has a title and a short description
+		
+		#clicking puts focus on the selected option
+		
+		#a "submit" button appears"
+		
+		#user presses submit button
+		
+		#user is shown the vote count for those 2 items. 
+		
+		#user is presented with 2 new options
+if __name__ == "__main__":
+	unittest.main(warnings="ignore")

334a0caf5a9d7eed828096163fc809622266e995
Sam Kamenetz <samkamenetz@gmail.com>
2019-06-07 22:02:48 -0400

    created voting app, created homepage view

---
+++ voting/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import resolve
+from voting.views import home_page
+class ViewTest(TestCase):
+	def test_renders_home_page(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
+	def test_home_page_returns_correct_html(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
